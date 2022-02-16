180d6dd28bc98c13d374bbabdb7a931f0bfd60e4
zsunkim <jisun124918@gmail.com>
2022-02-16 14:37:37 +0900

    ws 서버 연결

--- src/server.js
+++ src/server.js
@@ -8,7 +8,7 @@ app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (_, res) => res.render("home"));
-app.get("/*", (_, res) => res.render("/"));
+app.get("/*", (_, res) => res.redirect("/"));
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 
@@ -18,7 +18,6 @@ const wss = new WebSocket.Server({ server });
 function handleConnection(socket) {
   console.log(socket);
 }
+wss.on("connection", handleConnection);
 
-wss.on("connection", handleConnection)
-
-server.listen(3000, handleListen)
+server.listen(3000, handleListen);
