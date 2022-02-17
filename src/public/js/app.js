4766ff0c59647519f42cd5ada1d68ec20c9b4ba4
zsunkim <jisun124918@gmail.com>
2022-02-17 09:15:10 +0900

    webSocket message test

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,20 @@
 const socket = new WebSocket(`ws://${window.location.host}`);
+
+// socker open event // connection이 open되면 메소드 실행
+socket.addEventListener("open", () => {
+  console.log("Connected to Server");
+});
+
+// socker message event // 메세지를 받을 때마다 내용을 출력하는 이벤트
+socket.addEventListener("message", (message) => {
+  console.log("message: ", message.data);
+})
+
+// socker close event
+socket.addEventListener("close", () => {
+  console.log("Disconnected to Server");
+})
+
+setTimeout(() => {
+  socket.send("hello");
+}, 10000)
--- src/server.js
+++ src/server.js
@@ -15,9 +15,14 @@ const handleListen = () => console.log(`Listening on http://localhost:3000`);
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
-function handleConnection(socket) {
-  console.log(socket);
-}
-wss.on("connection", handleConnection);
+// backend에 연결된 사람의 정보 제공 (socket)
+wss.on("connection", (socket) => { // connection이 생겼을 때 socket으로 즉시 메세지 보내기
+  console.log("Connected to Browser");
+  socket.on("close", () => console.log("Disconnected from the Browser"));
+  socket.on("message", (message) => {
+    console.log(message.toString('utf-8'));
+  });
+  socket.send("hello!!!"); // socket으로 메세지 보내기
+});
 
 server.listen(3000, handleListen);
