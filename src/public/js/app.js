76cacb0da116b168dc271e7db243585438e60364
zsunkim <jisun124918@gmail.com>
2022-02-20 21:10:42 +0900

    add nickname

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,6 +1,12 @@
 const messageList = document.querySelector("ul");
-const messageForm = document.querySelector("form");
+const nickForm = document.querySelector("#nick");
+const messageForm = document.querySelector("#message");
 const socket = new WebSocket(`ws://${window.location.host}`);
+
+function makeMessage(type, payload) {
+  const msg = { type, payload };
+  return JSON.stringify(msg);
+}
 
 // socker open event // connection이 open되면 메소드 실행
 socket.addEventListener("open", () => {
@@ -9,7 +15,9 @@ socket.addEventListener("open", () => {
 
 // socker message event // 메세지를 받을 때마다 내용을 출력하는 이벤트
 socket.addEventListener("message", (message) => {
-  console.log("message: ", message.data);
+  const li = document.createElement("li");
+  li.innerText = message.data;
+  messageList.append(li);
 })
 
 // socker close event
@@ -20,8 +28,15 @@ socket.addEventListener("close", () => {
 function handleSubmit(event) {
   event.preventDefault();
   const input = messageForm.querySelector("input");
-  socket.send(input.value);
+  socket.send(makeMessage("new_message", input.value));
   input.value = "";
 }
 
+function handleNickSubmit(event) {
+  event.preventDefault();
+  const input = nickForm.querySelector("input");
+  socket.send(makeMessage("nickname", input.value));
+}
+
 messageForm.addEventListener("submit", handleSubmit);
+nickForm.addEventListener("submit", handleNickSubmit);
